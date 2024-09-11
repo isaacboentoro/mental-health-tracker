91e6d5298b6ac8e861adfafb6822b9efca939080
isaacboentoro <isaac.jesse@ui.ac.id>
2024-09-11 11:01:06 +0700

    Commplete Tutorial 2 (Add MoodEntry form and autodeployment to PWS)

--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # add this line
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
@@ -11,3 +12,4 @@ class MoodEntry(models.Model):
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
+
