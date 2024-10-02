c255ed1b4b9bfb3fb24f53b781b5ecd57a004d9b
isaacboentoro <isaac.jesse@ui.ac.id>
2024-10-02 10:43:12 +0700

    finish tutorial 5

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,7 @@ from main.views import (
         logout_user,
         edit_mood,
         delete_mood,
+        add_mood_entry_ajax
 )
 
 app_name = 'main'
@@ -27,4 +28,5 @@ urlpatterns = [
         path('logout/', logout_user, name='logout'),
         path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
         path('delete/<uuid:id>', delete_mood, name='delete_mood'),
+        path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
 ]
